395c4e28597e425807f9389f0b99cd5d59acde42
yiyu0x <tako0903@icloud.com>
2019-03-18 16:37:32 +0800

    first commit

---
+++ Lab1/Flashing.c
@@ -0,0 +1,47 @@
+#include <8051.h>
+// p0 : 18 ~ 25
+void timer() {
+	unsigned d1, d2;
+	for (d1 = 0; d1 < 1024; d1 ++) {
+		for (d2 = 0; d2 < 100; d2 ++) {
+		}
+	}
+}
+void main()
+{
+	unsigned led;
+	led = 16; // 12 O'clock
+	int CASE = 0;
+	while (1) {
+		if (CASE == 0) {
+			led >>= 1;
+			P1 = ~led;
+			if (led == 0) {
+				CASE = 1;
+				led = 256;
+				P1 = 255; // turn all off
+			}
+		}
+		if (CASE == 1) {
+			led >>= 1;
+			P0 = ~led;
+			if (led == 0) {
+				CASE = 2;
+				led = 256;
+				P0 = 255; // turn all off
+			}
+		}
+		if (CASE == 2) {
+			led >>= 1;
+			P2 = ~led;
+			if (led == 0) {
+				CASE = 0;
+				led = 256;
+				P2 = 255;
+				continue; // turn all off
+			}
+		}
+		
+		timer();
+	}
+}
---
+++ Lab2/Flashing.c
@@ -0,0 +1,43 @@
+#include <8051.h>
+#include <stdbool.h>
+// p0 : 18 ~ 25
+
+void delay(short time) {
+	unsigned short d1;
+	for (d1 = 0; d1 < time*100; d1 ++) {
+	}
+}
+void main() {
+	short table[] = {0x75, 0xb0};
+	bool index = 0;
+	bool flag_plus = 1;
+	bool flag_minus = 1;
+	while (1) {
+		
+		index = !index;
+		P1 = table[index];
+		delay(50);
+
+		if (P0_0 == 0 && flag_minus) {
+			if (table[0] == 0x70 && table[1] == 0xb0) continue;
+			flag_minus = 0;
+			table[1] -= 1;
+			if (table[1] == 0xaf) {
+				table[1] = 0xb9;
+				table[0]--;
+			}
+		}
+		else if (P0_0 == 1) flag_minus = 1;
+		//
+		if (P0_1 == 0 && flag_plus) {
+			if (table[0] == 0x79 && table[1] == 0xb9) continue;
+			flag_plus = 0;
+			table[1] += 1;
+			if (table[1] == 0xba) {
+				table[1] = 0xb0;
+				table[0]++;
+			}
+		}
+		else if (P0_1 == 1) flag_plus = 1;
+	}
+}
